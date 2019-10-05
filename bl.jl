c50485c700b58033263c752ff2be798cdbb49272
Tobias Brodd <tobias.brodd@gmail.com>
2019-10-05 16:54:23 +0200

    added black-litterman allocation model

---
+++ bl.jl
@@ -0,0 +1,48 @@
+module BL
+
+# Black-Litterman Model
+
+include("mpt.jl")
+include("utils.jl")
+include("capm.jl")
+
+using Statistics, .MPT, .CAPM, .Utils
+
+export allocation
+
+# Black-Litterman
+function black_litterman(P, Q, sigma, theta, tau)
+    sigma_mu_inv = inv(tau * sigma)
+    P_transpose = transpose(P)
+    c = 1 / tau
+    omega = (1 / c) * P * sigma * P_transpose
+
+    mu_bl = inv(sigma_mu_inv + P_transpose * omega * P) * (sigma_mu_inv * theta + P_transpose * omega * Q)
+    sigma_bl = (1 + tau) * sigma - tau^2 * sigma * P_transpose * inv(tau * P * sigma * P_transpose + omega) * P * sigma
+
+    return mu_bl, sigma_bl
+end
+
+# Portfolio Allocation 
+function allocation(R_historic, R_market, risk_free_rate, P, Q, tau, w)
+    lambda = risk_aversion_coefficient(R_market, risk_free_rate)
+    sigma = sample_sigma(R_historic)
+    theta = implied_returns(R_market, risk_free_rate, sigma, w)
+    mu_bl, sigma_bl = black_litterman(P, Q, sigma, theta, tau)
+    
+    return mean_variance(1, lambda, mu_bl, sigma_bl; r = risk_free_rate)
+end
+
+function test()
+    R_historic = [1.0 1.0; 1.3 1.2; 1.5 1.1]
+    R_market = [1.0 1.2 1.3]
+    risk_free_rate = 1.05
+    P = [1 0; 0 1]
+    Q = [1.5; 1.0]
+    tau = 0.05
+    w = [0.5; 0.5]
+
+    return allocation(R_historic, R_market, risk_free_rate, P, Q, tau, w)
+end
+
+end
