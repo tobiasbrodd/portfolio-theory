a6dca9c0416b9e0d6d3e95b02510b690cfa579fc
Tobias Brodd <tobias.brodd@gmail.com>
2019-10-05 16:54:04 +0200

    added CAPM implied returns function

---
+++ capm.jl
@@ -0,0 +1,18 @@
+module CAPM
+
+# CAPM
+
+include("utils.jl")
+
+using Statistics, .Utils
+
+export implied_returns
+
+# Implied Returns (CAPM)
+function implied_returns(R, r, sigma, w)
+    lambda = risk_aversion_coefficient(R, r)
+
+    return lambda * sigma * w
+end
+
+end
