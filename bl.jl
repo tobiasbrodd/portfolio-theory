d8cebc83c09931d795420131258ff9bdebbe8bba
Tobias Brodd <tobias.brodd@gmail.com>
2020-09-05 10:55:19 +0200

    renamed functions

--- bl.jl
+++ bl.jl
@@ -8,10 +8,27 @@ include("capm.jl")
 
 using Statistics, LinearAlgebra, .MPT, .CAPM, .Utils
 
-export allocate
+export black_litterman
 
 # Black-Litterman
-function black_litterman(P, Q, sigma, theta, tau)
+function black_litterman(R_historic, R_market, risk_free_rate, P, Q, tau, w; long = false)
+    lambda = risk_aversion_coefficient(R_market, risk_free_rate)
+    sigma = cov(R_historic)
+    theta = implied_returns(R_market, risk_free_rate, sigma, w)
+    mu_bl, sigma_bl = estimate(P, Q, sigma, theta, tau)
+
+    println("CAPM: ", round(mean(theta) * 100, digits=2), "%")
+    println("Black-Litterman: ", round(mean(mu_bl) * 100, digits=2), "%")
+    
+    if long
+        return mean_variance_long(1, abs(lambda), mu_bl, sigma_bl; r = risk_free_rate)
+    else
+        return mean_variance(1, abs(lambda), mu_bl, sigma_bl; r = risk_free_rate)
+    end
+end
+
+# Estimate mean and standard deviation
+function estimate(P, Q, sigma, theta, tau)
     tau_sigma_inv = inv(tau * sigma)
     P_transpose = transpose(P)
     c = 1 / tau
@@ -27,23 +44,6 @@ function black_litterman(P, Q, sigma, theta, tau)
     return mu_bl, sigma_bl
 end
 
-# Allocate portfolio 
-function allocate(R_historic, R_market, risk_free_rate, P, Q, tau, w; long = false)
-    lambda = risk_aversion_coefficient(R_market, risk_free_rate)
-    sigma = cov(R_historic)
-    theta = implied_returns(R_market, risk_free_rate, sigma, w)
-    mu_bl, sigma_bl = black_litterman(P, Q, sigma, theta, tau)
-
-    println("CAPM: ", round(mean(theta) * 100, digits=2), "%")
-    println("Black-Litterman: ", round(mean(mu_bl) * 100, digits=2), "%")
-    
-    if long
-        return mean_variance_long(1, abs(lambda), mu_bl, sigma_bl; r = risk_free_rate)
-    else
-        return mean_variance(1, abs(lambda), mu_bl, sigma_bl; r = risk_free_rate)
-    end
-end
-
 function test()
     R_historic = [0.5 -0.3; 0.6 -0.2; 0.7 0.1]
     R_market = [0.1 0.2 0.3]
@@ -53,7 +53,7 @@ function test()
     tau = 0.05
     w = [0.5; 0.5]
 
-    return allocate(R_historic, R_market, risk_free_rate, P, Q, tau, w)
+    return black_litterman(R_historic, R_market, risk_free_rate, P, Q, tau, w)
 end
 
 end
